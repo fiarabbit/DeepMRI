781e674a2f462a0ee904df276d2daf471c24fc05
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-08-21 17:33:28 +0900

    reverted model

--- model.py
+++ model.py
@@ -14,12 +14,14 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
             self.conv2 = L.ConvolutionND(3, 16, 32, (5, 7, 5), 2, 0)
             self.conv3 = L.ConvolutionND(3, 32, 64, (7, 8, 7), 2, 0)
             self.conv4 = L.ConvolutionND(3, 64, 128, (8, 9, 8), 1, 0)
-            self.l1 = L.Linear(None, 250)
-            self.bn1 = L.BatchNormalization(250)
+            self.l1 = L.Linear(None, 150)
+            self.bn1 = L.BatchNormalization(150)
             self.l2 = L.Linear(None, 20)
             self.bn2 = L.BatchNormalization(20)
-            self.l3 = L.Linear(None, 128)
-            self.bn3 = L.BatchNormalization(128)
+            self.l3 = L.Linear(None, 150)
+            self.bn3 = L.BatchNormalization(150)
+            self.l4 = L.Linear(None, 128)
+            self.bn4 = L.BatchNormalization(128)
             self.deconv4 = L.DeconvolutionND(3, 128, 64, (8, 9, 8), 1, 0)
             self.deconv3 = L.DeconvolutionND(3, 64, 32, (7, 8, 7), 2, 0)
             self.deconv2 = L.DeconvolutionND(3, 32, 16, (5, 7, 5), 2, 0)
@@ -27,7 +29,7 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
 
     def calc(self, x):
         _shape = list(x.shape)
-        _shape.insert(1, 1) # specify # of first channel
+        _shape.insert(1, 1)  # specify # of first channel
 
         c0 = F.reshape(x, tuple(_shape))
         c1 = F.tanh(self.conv1(c0))
@@ -38,7 +40,8 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
         l1 = F.tanh(self.bn1(self.l1(c4)))
         l2 = F.tanh(self.bn2(self.l2(l1)))
         l3 = F.tanh(self.bn3(self.l3(l2)))
-        b4 = F.reshape(l3,(l3.shape[0], 128, 1, 1, 1))
+        l4 = F.tanh(self.bn4(self.l4(l3)))
+        b4 = F.reshape(l4, (l4.shape[0], 128, 1, 1, 1))
         b3 = F.tanh(self.deconv4(b4))
         b2 = F.tanh(self.deconv3(b3))
         b1 = F.tanh(self.deconv2(b2))
