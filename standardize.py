74e601982de5c46f7e8925eb5b6b1b332ea83ee7
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-09-11 12:31:24 +0900

    last version of standardization

--- standardize.py
+++ standardize.py
@@ -5,6 +5,8 @@ import nibabel as nib
 import numpy as np
 import numpy.ma as ma
 
+import re
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,16 +15,29 @@ def main():
     parser.add_argument('--result', default='/data2/preprocessed_data')
     args = parser.parse_args()
     mask = nib.load(args.mask).get_data()
+    mask = np.reshape(mask, list(mask.shape) + [1])
     targets = os.listdir(args.target)
-    for target in targets:
+    for i in range(len(targets)):
+        target = targets[i]
+        print("processing {}/{}".format(i, len(targets)))
         t = os.path.join(args.target, target)
         x = nib.load(t).get_data()
+        if mask.shape != x.shape:
+            mask = np.broadcast_to(mask, x.shape)
         x_masked = ma.masked_where(mask, x)
+        _x_mean = np.mean(x_masked, axis=x_masked.ndim-1)
+        x_mean = np.reshape(_x_mean, list(_x_mean.shape)+[1])
         x_voxel \
-            = x_masked - np.mean(x_masked, axis=x_masked.shape[-1])
+            = x_masked - x_mean
         x_standardized = (x_voxel - np.mean(x_voxel)) / np.std(x_voxel)
-        np.savez_compressed(
-            os.path.join(args.result, target), dataobj=x_standardized)
+        assert isinstance(x_standardized, ma.MaskedArray)
+        save_name = re.sub("$", ".npz", target)
+        save_path = os.path.join(args.result, save_name)
+        print(save_path)
+        np.savez_compressed(save_path,
+                            mask=x_standardized.mask, data=x_standardized.data)
+        # x_standardized.dump(os.path.join(args.result, save_name))
+
 
 
 if __name__ == '__main__':
