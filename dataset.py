9dad09f99767dc119d7b53472e243604869d0393
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-09-11 17:44:31 +0900

    fixed data split & npz

--- dataset.py
+++ dataset.py
@@ -2,14 +2,16 @@ from chainer.dataset import DatasetMixin
 from chainer.datasets import split_dataset
 
 import nibabel as nib
+import numpy as np
 from os import listdir
 from os import path
 import re
+import math
 
 
 class TimeSeriesAutoEncoderDataset(DatasetMixin):
     regexp = re.compile(
-        'niftiDATA_Subject(?P<sid>\d{3})_Condition(?P<cid>\d{3}).nii')
+        'niftiDATA_Subject(?P<sid>\d{3})_Condition(?P<cid>\d{3})\.nii\.npz$')
     frame_number = 150
 
     def __init__(self, root, split_inter=True, subsampling=True,
@@ -21,43 +23,54 @@ class TimeSeriesAutoEncoderDataset(DatasetMixin):
         self.subject_number = len(self.subjects)
         self.split_inter = split_inter
         self.subsampling = subsampling
-        self.split_ratio = split_ratio
+        self.split_ratio_tuple = split_ratio
 
     def __len__(self):
         return self.subject_number * self.frame_number
 
     def get_example(self, i):
-        if self.split_inter:
+        if not self.split_inter:
             frame, subject = divmod(i, self.subject_number)
         else:
             subject, frame = divmod(i, self.frame_number)
 
         frame = int(frame)
         filepath = path.join(
-            self.root, "niftiDATA_Subject{}_Condition000.nii"
+            self.root, "niftiDATA_Subject{}_Condition000.nii.npz"
                 .format(self.subjects[subject]))
-        img = nib.load(filepath)
-        npimg = img.dataobj[:, :, :, frame]
-        return npimg
+        loaded_dict = np.load(filepath)
+        img = loaded_dict["data"]
+        mask = loaded_dict["mask"]
+        npimg = img[:, :, :, frame]
+        return npimg, mask
 
     def get_subdatasets(self):
-        order = []
-        if self.split_inter:
-            if self.subsampling:
-                for i in range(0, len(self)):
-                    frame, subject = divmod(i, self.subject_number)
-                    if (frame % sum(self.split_ratio)) < self.split_ratio[0]:
-                        order.append(i)
-            else:
-                order = list(
-                    range(self.subject_number * self.split_ratio[0] // sum(
-                        self.split_ratio) * self.frame_number))
+        train = []
+        train_ratio_int = self.split_ratio_tuple[0]
+        ratio_sum_int = sum(self.split_ratio_tuple)
+        if not self.split_inter:
+            for i in range(0, len(self)):
+                frame, subject = divmod(i, self.subject_number)
+                if self.subsampling:
+                    if (frame % ratio_sum_int) < train_ratio_int:
+                        train.append(i)
+                else:
+                    threshold = math.ceil(
+                        self.frame_number * train_ratio_int / ratio_sum_int
+                    )
+                    if frame < threshold:
+                        train.append(i)
         else:
-            order = list(range(self.frame_number * self.split_ratio[0] // sum(
-                self.split_ratio) * self.subject_number))
-        split_at = len(order)
+            for i in range(0, len(self)):
+                subject, frame = divmod(i, self.frame_number)
+                threshold = math.ceil(
+                        self.subject_number * train_ratio_int / ratio_sum_int
+                    )
+                if subject < threshold:
+                    train.append(i)
+        split_at = len(train)
         assert (split_at != 0) & (split_at != len(self))
+        valid = set(range(len(self))) - set(train)
+        train.extend(valid)
 
-        order.extend(set(range(len(self))) - set(order))
-
-        return split_dataset(self, split_at, order)
+        return split_dataset(self, split_at, train)
