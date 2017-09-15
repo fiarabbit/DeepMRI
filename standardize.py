50fb05f5e7bb2da3890e99b48bf2ce2ec0985ce6
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-09-15 13:36:17 +0900

    inversed mask flag (fix)

--- standardize.py
+++ standardize.py
@@ -17,7 +17,7 @@ def main():
     args = parser.parse_args()
     mask = nib.load(args.mask).get_data()
     assert isinstance(mask, np.ndarray)
-    mask = (mask != 0)
+    mask = (mask == 0)
     mask = np.reshape(mask, list(mask.shape) + [1])
     targets = os.listdir(args.target)
     for i in range(len(targets)):
