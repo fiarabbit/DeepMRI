2d887dc75dae807eee7ac622cdcd61590b4d3ff6
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-11-18 21:49:17 +0900

    fixed

--- model.py
+++ model.py
@@ -5,37 +5,12 @@ import chainer.links as L
 
 
 class ThreeDimensionalAutoEncoder(chainer.Chain):
-    in_size = (91, 109, 91) # = 902629
+    in_size = (91, 109, 91) # = 902629 (effective number == mask == 150350)
 
     def __init__(self, mask):
         assert tuple(mask.shape) == self.in_size
         super().__init__()
         self.mask = chainer.Variable(mask)
-        with self.init_scope():
-            self.conv1 = L.ConvolutionND(3, 1, 3, (7, 7, 7), 2, 0)
-            # (3, 43, 52, 43) = 288444 = 32%
-            self.bnc1 = L.BatchNormalization(3)
-            self.conv2 = L.ConvolutionND(3, 3, 10, (5, 6, 5), 2, 0)
-            # (10, 20, 24, 20) = 96000 = 33%
-            self.bnc2 = L.BatchNormalization(10)
-            self.conv3 = L.ConvolutionND(3, 10, 35, (4, 4, 4), 2, 0)
-            # (35, 9, 11, 9) = 31185 = 32.5%
-            self.bnc3 = L.BatchNormalization(35)
-            self.conv4 = L.ConvolutionND(3, 35, 125, (3, 3, 3), 2, 0)
-            # (125, 4, 5, 4) = 10000 = 32%
-            self.bnc4 = L.BatchNormalization(125)
-            self.deconv4 = L.DeconvolutionND(3, 125, 35, (3, 3, 3), 2, 0)
-            # (35, 9, 11, 9)
-            self.bnd4 = L.BatchNormalization(35)
-            self.deconv3 = L.DeconvolutionND(3, 35, 10, (4, 4, 4), 2, 0)
-            # (10, 20, 24, 20)
-            self.bnd3 = L.BatchNormalization(10)
-            self.deconv2 = L.DeconvolutionND(3, 10, 3, (5, 6, 5), 2, 0)
-            # (3, 43, 52, 32)
-            self.bnd2 = L.BatchNormalization(3)
-            self.deconv1 = L.DeconvolutionND(3, 3, 1, (7, 7, 7), 2, 0)
-            # (1, 91, 109, 91)
-            self.bnd1 = L.BatchNormalization(1)
 
     def to_cpu(self):
         super().to_cpu()
@@ -47,19 +22,8 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
 
     def extract(self, x):
         _shape = list(x.shape)
-        try:
-            assert tuple(_shape[1:]) == self.in_size
-        except AssertionError:
-            print("expected:{}, actual:{}".format(self.in_size, tuple(_shape[1:])))
-            exit()
-
-        _shape.insert(1, 1)  # specify # of first channel
-
-        c0 = F.reshape(x, tuple(_shape))
-        c1 = F.relu(self.bnc1(self.conv1(c0)))
-        c2 = F.relu(self.bnc2(self.conv2(c1)))
-        c3 = F.relu(self.bnc3(self.conv3(c2)))
-        return self.conv4(c3)
+        xp = chainer.cuda.get_array_module(x)
+        return chainer.Variable(xp.zeros(_shape[0]))
 
     def calc(self, x):
         _shape = list(x.shape)
@@ -68,23 +32,8 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
         except AssertionError:
             print("expected:{}, actual:{}".format(self.in_size, tuple(_shape[1:])))
             exit()
-
-        _shape.insert(1, 1)  # specify # of first channel
-
-        c0 = F.reshape(x, tuple(_shape))
-        c1 = F.relu(self.bnc1(self.conv1(c0)))
-        c2 = F.relu(self.bnc2(self.conv2(c1)))
-        c3 = F.relu(self.bnc3(self.conv3(c2)))
-        c4 = F.relu(self.bnc4(self.conv4(c3)))
-        b3 = F.relu(self.bnd4(self.deconv4(c4)))
-        b2 = F.relu(self.bnd3(self.deconv3(b3)))
-        b1 = F.relu(self.bnd2(self.deconv2(b2)))
-        y = F.relu(self.bnd1(self.deconv1(b1)))
-
-        _shape = list(y.shape)
-        _shape.pop(1)
-
-        return F.reshape(y, tuple(_shape))
+        xp = chainer.cuda.get_array_module(x)
+        return chainer.Variable(xp.zeros(_shape))
 
     def __call__(self, x):
         x_masked = F.scale(x, self.mask, axis=1)
