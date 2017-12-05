93aff491878df39d69f6604b7297ce43cde0e956
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-12-05 15:47:48 +0900

    fixed - use LeCunUniform as initializer - 20 epoch

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ class ThreeDimensionalAutoEncoder(chainer.Chain):
             self.xp.array(mask != 0, dtype=self.xp.float32))
         assert tuple(mask.shape) == self.in_size
 
+        print(type(self.mask.data))
         self.idx_mask = self.xp(self.mask.data.nonzero())
         assert isinstance(self.idx_mask, tuple)
 
