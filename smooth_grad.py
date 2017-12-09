83f390cd0e07a88cd14ce4fe787ae2e58d7c12d0
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2017-12-10 03:57:04 +0900

    smooth_grad

--- smooth_grad.py
+++ smooth_grad.py
@@ -8,6 +8,7 @@ from chainer import iterators
 
 from argparse import ArgumentParser
 
+feature_size = 1000
 
 def main():
     parser = ArgumentParser()
@@ -23,7 +24,7 @@ def main():
 
     all_dataset = _dataset.TimeSeriesAutoEncoderDataset(args.datasetdir,
                                                         split_inter=args.split_inter)
-    train_dataset, test_dataset = all_dataset.get_subdatasets()
+    _, test_dataset = all_dataset.get_subdatasets()
     test_itr = iterators.SerialIterator(dataset=test_dataset,
                                         batch_size=1,
                                         repeat=False, shuffle=False)
@@ -32,12 +33,12 @@ def main():
         target_img = next(test_itr)[0]
 
     # preprocessing
-    batch = np.copy(np.broadcast_to(target_img,
-                            [args.testBatchsize] + list(target_img.shape)))
+    batch_size = args.testBatchsize
+    batch = np.broadcast_to(target_img,
+                            [batch_size] + list(target_img.shape))
     noise_level = 0.2
     sigma = noise_level / (np.max(target_img) - np.min(target_img))
-    for i in range(0, args.testBatchsize):
-        batch[i, :] += sigma * np.random.randn(*batch[i, :].shape)
+    batch += sigma * np.random.randn(*batch.shape)
 
     mask = np.array(nib.load(args.mask).get_data(), dtype=np.float32)
     model = _model.ThreeDimensionalAutoEncoder(mask)
@@ -48,13 +49,15 @@ def main():
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
         batch = chainer.cuda.to_gpu(batch, args.gpu)
+
     x = chainer.Variable(batch)
     feature = model.extract(x)
-    _feature = chainer.functions.sum(chainer.functions.get_item(feature, [..., 0]))
+    assert feature.shape == (batch_size, 1, 1, 1)
+    feature_coordinate = (0, 0, 0)
+    _feature = chainer.functions.get_item(feature, [Ellipsis] + list(feature_coordinate))
     _feature.backward()
     filename = 'grad.npz'
     np.savez_compressed(filename, grad=chainer.cuda.to_cpu(x.grad))
-
 
 if __name__ == '__main__':
     main()
